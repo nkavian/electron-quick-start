3b616dfb5bcbf4191a241be84d2eaa6acb6b068b
Nas Kavian <>
2021-02-14 12:41:05 -0800

    Using the electron quick start with two Webtorrents hangs.

--- main.js
+++ main.js
@@ -30,6 +30,20 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+
+
+  const WebTorrent = require('webtorrent-hybrid')
+  var client = new WebTorrent()
+  var torrentId1 = 'magnet:?xt=urn:btih:5f342743106c95a5bafb8b06c84b9774ffaae502&dn=test-wt3&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com'
+  client.add(torrentId1, function (torrent) {
+    console.log('torrent 1 added')
+  })
+  var torrentId2 = 'magnet:?xt=urn:btih:15d7fb1905e3dc648472d394b1c77a0cb682c02e&dn=test-wt2&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com'
+  client.add(torrentId2, function (torrent) {
+    console.log('torrent 2 added')
+  })
+
+
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
